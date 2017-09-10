244efce07ed5f659e9dab6390e6794c857016b6f
Carlie Ostrom <rlietrom@gmail.com>
2017-09-10 01:25:21 -0400

    made globe in threej s

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-
+var globe = require('./globe')
 var index = require('./routes/index');
 
 var app = express();
@@ -9,11 +9,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+
 // app.get('/', function (req, res) {
 //   res.render('home')
 // })
 
 app.use('/', index);
+
+// app.use('/', globe);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -25,7 +30,6 @@ app.use(function(req, res, next) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-
 
 // error handlers
 
